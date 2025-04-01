07ff51844fc9ab98bdce78a2d03ca0b218a4ebea
rishavch0udhary <rishavchoudharysupercell@gmail.com>
2025-04-01 19:18:46 +0530

    function

--- Array/ArrayCC.java
+++ Array/ArrayCC.java
@@ -2,8 +2,27 @@
 
 public class ArrayCC {
 
+    // public static void main(String[] args) {
+    //     System.out.println("Hiii");
+    //     int marks[] = new int[50];
+    //     int numbers[] = {1,2,3};
+    //     String fruits[] = {"apple","mango"};
+    // }
+public static void update(int marks[],int nonchangable) {
+    nonchangable = 19;
+    for (int i = 0; i < marks.length; i++) {
+        marks[i] = marks[i]+1;
+    }
+}
     public static void main(String[] args) {
-        System.out.println("Hiii");
-        
+        int marks[] = {97,98,99};
+        int nonchangable = 5;
+        update(marks,nonchangable);
+System.out.println(nonchangable);
+        // print our marks
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i]+" ");
+        }
+        System.out.println();
     }
 }
